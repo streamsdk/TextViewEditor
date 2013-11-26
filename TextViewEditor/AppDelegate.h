2fee6d3a952ffbdd8dd648057da5b19f42c15fe0
edwardyangey <wangsh@wangshtekiiMac.local>
2013-11-26 13:29:59 +0800

    Initial Commit

---
+++ TextViewEditor/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TextViewEditor
+//
+//  Created by wangsh on 13-11-26.
+//  Copyright (c) 2013年 wangsh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TextViewEditor/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TextViewEditor
+//
+//  Created by wangsh on 13-11-26.
+//  Copyright (c) 2013年 wangsh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
